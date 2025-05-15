d812670fcdb512116ef8e33735274c602ae9a322
Alan Edwin <alansedwin@gmail.com>
2025-05-15 16:24:35 +0200

    fixed tile things

--- tile.py
+++ tile.py
@@ -1,16 +1,15 @@
 import pygame, csv, os
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, image, x, y, world):
+    def __init__(self, image, x, y, speed):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect(topleft=(x, y))
-        self.speed = 0.0
-        self.world = world
+        self.speed = speed
+
 
     def update(self, *args):
-        self.rect.x -= self.world.GROUND_SPEED
-        self.speed = self.world.GROUND_SPEED
+        self.rect.x -= self.speed
         if self.rect.right < 0:
             self.kill()
 
